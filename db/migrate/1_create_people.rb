07383912893cad2342053531c0dbe5409c31e3bb
Jesse Wolfe <jes5199@gmail.com>
2015-11-07 22:35:07 -0800

    create people

---
+++ db/migrate/1_create_people.rb
@@ -0,0 +1,14 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name, null: false, index: true
+      t.string :email, null: true, index: true
+      t.integer :clef_id, limit: 8, null:true, index: true
+      t.references :invited_by_person, index: true #, foreign_key: {to_table: :people} # wishing for Rails 5, f.k. manually below
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :people, :people, column: :invited_by_person_id
+  end
+end
